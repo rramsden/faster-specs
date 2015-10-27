b5d01228161956a0a38bf2b64a25d2970b7eac57
Richard Ramsden <richard@rramsden.ca>
2015-10-27 10:42:33 +0900

    Add knapsack

--- playlist-app/spec/spec_helper.rb
+++ playlist-app/spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require 'knapsack'
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
@@ -6,3 +8,5 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 end
+
+Knapsack::Adapters::RspecAdapter.bind if ENV['CI']
